ed454f182f43369b1d3f0d762f55667bb117e18c
ikd9684 <ikd9684@gmail.com>
2015-05-21 00:24:52 +0900

    フォルダ構成を修正。

--- Problem1/Properties/AssemblyInfo.cs
+++ Problem1/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // アセンブリに関する一般情報は以下の属性セットをとおして制御されます。
 // アセンブリに関連付けられている情報を変更するには、
 // これらの属性値を変更してください。
-[assembly: AssemblyTitle("FiveProgrammingProblems")]
+[assembly: AssemblyTitle("Problem1")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("FiveProgrammingProblems")]
+[assembly: AssemblyProduct("Problem1")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // 次の GUID は、このプロジェクトが COM に公開される場合の、typelib の ID です
-[assembly: Guid("0d7fe11c-8db9-4e10-a403-b383a460d3de")]
+[assembly: Guid("08154de2-6871-40d1-8145-9856994cde1a")]
 
 // アセンブリのバージョン情報は、以下の 4 つの値で構成されています:
 //
